7a7af9623563acef0656e0407efb8c4e5f1250b8
名前 <>
2020-03-09 13:26:40 +0000

    コード　課題ruby03

---
+++ if.rb
@@ -0,0 +1,31 @@
+n = 3
+if n == 3
+  puts "nは3です"
+end
+
+n = 4
+if n = 3 || n = 4
+  puts "3,4のどちらかです"
+end
+
+if n == 4
+  puts "nは3です"
+elsif puts "nは4です"
+end
+
+for n in ["こんにちは", "こんにちは", "こんにちは", "こんにちは", "こんにちは"]do
+  puts n
+end
+
+[1,2,3,4,5].each do |n|
+  puts n
+end
+
+n = 2
+if n == 2
+  puts "2です"
+elsif n == 3
+  puts "3です"
+else
+  puts "それ以外です"
+end
